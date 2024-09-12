88c633f5ae7c564ff741f774fabaa33954e61f05
yrm14 <107495212+yrm14@users.noreply.github.com>
2024-09-12 13:17:13 +0000

    datatypes return value

---
+++ Code_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// So there are 2 types of database in js :
+// 1.) Primitive and 2.) Non Primitive
+
+// Primitive - All datatypes are Call by value only . There are basically into 7 categories:(memory mae change nahi hota hae but copy banti hae aur uss copy me change hota hae)
+
+// 1.) String
+// 2.) Boolean
+// 3.) Number
+// 4.) null
+// 5.) undefined
+// 6.) Symbol
+// 7.) Bigint
+
+// All non primitive - All are call by reference only . These are into 3 categories: (memory mae change hoga)
+
+// 1.) Arrays
+// 2.) Objects
+// 3.) Functions
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+
